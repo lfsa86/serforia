20d167bb33e628ab50e0ebc884bea45125b0eedd
Lautaroorbes <lautaro.orbes@lilab.pe>
2025-12-12 16:46:51 -0300

    CLEAN REPO

--- demo-db/fix-entrypoint.ps1
+++
@@ -1,4 +0,0 @@
-$path = 'D:\D-Gilmer\lilab\DIN\naturai-serfor-demo\init\entrypoint.sh'
-$text = [System.IO.File]::ReadAllText($path) -replace "`r",''
-[System.IO.File]::WriteAllText($path, $text, (New-Object System.Text.UTF8Encoding($false)))
-Write-Host "File converted to Unix format without BOM"
